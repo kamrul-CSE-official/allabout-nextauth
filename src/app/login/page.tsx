f1bb06cd6a59d2cad4205fe5420b805e52887f63
MD.Kamrul Hasan ⚡️ <kamrulhasan635653@gmail.com>
2024-10-28 22:12:00 +0600

    Added signup and signIn from

---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import LoginForm from "@/components/pageComponents/loginForm";
+import React from "react";
+
+const LoginPage = () => {
+  return (
+    <div className="container mx-auto animate-bump overflow-hidden">
+      <LoginForm />
+    </div>
+  );
+};
+
+export default LoginPage;
---
+++ src/components/pageComponents/loginForm.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Github, Mail, Loader2 } from "lucide-react";
+import Link from "next/link";
+
+export default function LoginForm() {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  async function onSubmit(event: React.SyntheticEvent) {
+    event.preventDefault();
+    setIsLoading(true);
+
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }
+
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <Card>
+        <h3 className="text-xl font-bold text-center m-2">
+          All about <span className="text-purple-700">next auth</span>
+        </h3>
+        <CardHeader className="space-y-1">
+          <CardTitle className="text-2xl">Login</CardTitle>
+          <CardDescription>
+            Enter your email and password to login
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="grid gap-4">
+          <div className="grid grid-cols-2 gap-6">
+            <Button variant="outline">
+              <Github className="mr-2 h-4 w-4" />
+              GitHub
+            </Button>
+            <Button variant="outline">
+              <Mail className="mr-2 h-4 w-4" />
+              Gmail
+            </Button>
+          </div>
+          <div className="relative">
+            <div className="absolute inset-0 flex items-center">
+              <span className="w-full border-t" />
+            </div>
+            <div className="relative flex justify-center text-xs uppercase">
+              <span className="bg-background px-2 text-muted-foreground">
+                Or continue with
+              </span>
+            </div>
+          </div>
+          <form onSubmit={onSubmit}>
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input id="email" type="email" placeholder="m@example.com" />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="password">Password</Label>
+              <Input id="password" type="password" />
+            </div>
+            <Button className="w-full mt-4" type="submit" disabled={isLoading}>
+              {isLoading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+              Login
+            </Button>
+          </form>
+        </CardContent>
+        <CardFooter>
+          <div className="text-sm text-center w-full">
+            Do not have an account?{" "}
+            <Link
+              href="/"
+              className="underline underline-offset-4 hover:text-primary"
+            >
+              Sign up
+            </Link>
+          </div>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
